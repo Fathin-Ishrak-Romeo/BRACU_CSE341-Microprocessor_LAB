43461f98aaed93dc9001ac139eae9c18b0023545
Fathin Ishrak Romeo <145606749+Fathin-Ishrak-Romeo@users.noreply.github.com>
2024-11-09 21:51:56 +0600

    Update task4(large from small)_lab1.asm

--- Lab 1/Assemble codes(Lab Tasks)/task4(large from small)_lab1.asm
+++ Lab 1/Assemble codes(Lab Tasks)/task4(large from small)_lab1.asm
@@ -7,8 +7,8 @@ main proc
     mov ax, @data
     mov ds, ax
     
-    mov ax, 1   ;a random value assigned to ax
-    mov bx, 2   ;a random value assigned to bx     
+    mov ax, 1   
+    mov bx, 2     
     
     ;subtracting bx (large) from ax (small)
     sub ax, bx  ;ax = ax - bx = 1 - 2 = -1
