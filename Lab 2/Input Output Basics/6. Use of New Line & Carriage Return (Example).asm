f500052ba331c03e70b57edd17e31f3d1ba59db5
Fathin Ishrak Romeo <145606749+Fathin-Ishrak-Romeo@users.noreply.github.com>
2024-12-07 22:16:54 +0600

    Update 6. Use of New Line & Carriage Return (Example).asm

--- Lab 2/Input Output Basics/6. Use of New Line & Carriage Return (Example).asm
+++ Lab 2/Input Output Basics/6. Use of New Line & Carriage Return (Example).asm
@@ -50,7 +50,7 @@ main proc
                   ;but our desired output is 1 (ascii 49)
 
     ;ascii to actual value conversion
-    add bl, 48    ;here 48 is add to the ascii form to get the actual value
+    add bl, 48    ;here 48 is added to the ascii form to get the actual value
                   ;in order to addtion operation, we would add to the subtracted result to convert ascii form the actual value
                                               
     
